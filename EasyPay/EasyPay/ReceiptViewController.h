8baa7c02d90e90bac8d60432435f2810723b2bb9
Henri Warren <henriwarren@Macintosh-2.local>
2012-08-26 19:55:43 -0700

    new controllers and a diagram to help me think

---
+++ EasyPay/EasyPay/ReceiptViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReceiptViewController.h
+//  EasyPay
+//
+//  Created by Hank Warren on 8/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReceiptViewController : UIViewController
+
+@end
---
+++ EasyPay/EasyPay/ReceiptViewController.m
@@ -0,0 +1,44 @@
+//
+//  ReceiptViewController.m
+//  EasyPay
+//
+//  Created by Hank Warren on 8/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ReceiptViewController.h"
+
+@interface ReceiptViewController ()
+
+@end
+
+@implementation ReceiptViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
